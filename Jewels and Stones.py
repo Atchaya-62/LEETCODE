b98546699af35496c261dc14f2ad7c5827bac44b
Atchaya-62 <atchayasaran626@gmail.com>
2024-07-28 22:54:35 +0530

    Create Jewels and Stones.py

---
+++ Jewels and Stones.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_set=set(jewels)
+        return sum(stone in jewels_set for stone in stones)
+        
